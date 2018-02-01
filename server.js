dafb45e694997c5d4e1446b94d47826c8e79241f
Caio Vieira <a.vieiracaio@gmail.com>
2018-02-01 01:28:35 -0200

    Morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const login = require('./routes/login')
 const user = require('./routes/user')
@@ -10,6 +11,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(morgan())
 
 app.use('/api/login', login(express))
 app.use('/api/user', user(express))
